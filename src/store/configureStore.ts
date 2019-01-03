3095b6f5e0fa37f6356d771c0628315858620d42
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-01-03 21:46:35 +0200

    Override store type to allow dispatching thunks

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, Store } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
