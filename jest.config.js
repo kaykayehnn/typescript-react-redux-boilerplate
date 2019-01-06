b4be94589d59015ac72eec7f815afc1e03be71af
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-01-06 02:08:04 +0200

    Remove __tests__ dir from base tsconfig, extract it to separate tsconfig

--- jest.config.js
+++ jest.config.js
@@ -60,7 +60,7 @@ module.exports = {
   // A set of global variables that need to be available in all test environments
   globals: {
     'ts-jest': {
-      'tsConfig': 'tsconfig.json'
+      'tsConfig': './__tests__/tsconfig-jest.json'
     }
   },
 
