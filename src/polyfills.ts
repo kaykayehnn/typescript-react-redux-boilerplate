8230edf96f273a36994a43ce59659f4d264eee3a
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-01-14 16:48:45 +0200

    Annotate polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,5 @@
+// FIXME: Depending on your requirements, you might want to import other polyfills:
+// fetch, Promise, etc.
 import 'core-js/fn/object/assign'
 import 'core-js/es6/set'
 import 'core-js/es6/map'
