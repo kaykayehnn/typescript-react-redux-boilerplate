a4bb94f21c3e7bca9c00132e9f2b24f1411fb292
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-01-20 14:13:06 +0200

    Fix paths

--- src/components/Layout/Layout.component.tsx
+++ src/components/Layout/Layout.component.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import { Link } from 'react-router-dom'
 
-import styles from './layout.style.scss'
+import styles from './Layout.style.scss'
 
 const Layout: FunctionComponent = ({ children }) => (
   <>
