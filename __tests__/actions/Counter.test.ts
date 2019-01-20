f76170a2f0010af27655afcecaafb59546538e36
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-01-20 13:21:29 +0200

    Fix filename capitalization
    Make git case sensitive with "git config core.ignorecase false"

---
+++ __tests__/actions/Counter.test.ts
@@ -0,0 +1,24 @@
+import { mockStore } from '../helpers'
+
+import { CounterTypes, incrementAsync } from '@Store/actions/Counter.actions'
+
+let store = mockStore()
+
+afterEach(() => {
+  store.clearActions()
+})
+
+describe('Counter Actions', () => {
+  it('incrementAsync works', () => {
+    const expected = [{
+      type: CounterTypes.INCREMENT
+    }]
+
+    store.dispatch(incrementAsync())
+    jest.advanceTimersByTime(999)
+    expect(store.getActions()).toEqual([])
+
+    jest.advanceTimersByTime(1)
+    expect(store.getActions()).toEqual(expected)
+  })
+})
