7d0e05644c6c45793f16e71926fdfc2915e01b7b
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-01-29 18:01:14 +0200

    Remove annotation

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,5 +1,3 @@
-// FIXME: Depending on your requirements, you might want to import other polyfills:
-// fetch, Promise, etc.
 import 'core-js/fn/object/assign'
 import 'core-js/es6/set'
 import 'core-js/es6/map'
