f02de5dc0660b12b08c52c9c1f22fd3c2a8b4e9c
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-02-05 22:02:04 +0200

    Update Redux types
    Remove DisconnectAction, add MergeProps

--- src/types/Redux.ts
+++ src/types/Redux.ts
@@ -1,6 +1,6 @@
-import { Store as StoreOriginal, Action, AnyAction, ActionCreator } from 'redux'
+import { Store as StoreOriginal, Action, AnyAction } from 'redux'
 import { ThunkDispatch, ThunkAction as ThunkActionOriginal } from 'redux-thunk'
-import { MapDispatchToProps as MapDispatchToPropsOriginal } from 'react-redux'
+import { MapDispatchToProps as MapDispatchToPropsOriginal, ResolveThunks } from 'react-redux'
 
 import AppState from '@Store/state/App.state'
 import AppActionTypes from '@Store/actions'
@@ -15,12 +15,6 @@ export type MapStateToProps<TStateProps, TOwnProps = {}> =
 export type MapDispatchToProps<TDispatchProps, TOwnProps = {}> =
   MapDispatchToPropsOriginal<TDispatchProps, TOwnProps>
 
-// Keeps parameter signature of function props but sets return value to any,
-// that way other components can utilize a not-connected version of that
-// subcomponent while maintaining strong typing and preserving semantics.
-// The reason we do this is because bound action creators return the action object,
-// which is usually not relevant to the component (the exceptions are thunks which
-// return promises but even then it is still uncommon).
-export type DisconnectAction<T extends ActionCreator<any>> = (...args: Parameters<T>) => any
+export type MergeProps<TStateProps, TDispatchProps = {}> = TStateProps & ResolveThunks<TDispatchProps>
 
 export type ThunkAction<R> = ThunkActionOriginal<R, AppState, undefined, AppActionTypes>
