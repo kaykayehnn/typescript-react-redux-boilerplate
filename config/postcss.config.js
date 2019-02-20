5ae80487fa8e812f8994a6d1a794d0df91d7b85b
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-02-20 23:45:37 +0200

    Update postcss config
    Removes all comments (including licenses) from CSS. The reasoning behind
    this is to encourage granular imports of libraries/frameworks

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    process.env.NODE_ENV === 'production' && require('cssnano')
+    process.env.NODE_ENV === 'production' && require('cssnano')({
+      preset: ['default', {
+        discardComments: {
+          removeAll: true
+        }
+      }]
+    })
   ].filter(Boolean)
 }
