f7e19627816dd66f57166e5cd759764661b004ab
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-03-04 12:20:36 +0200

    Replace tslint with eslint + prettier. Fixes #5
    Remove test command in lint-staged and add "eslint --fix"

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+const prettierConfig = require('./.prettierrc.js')
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  overrides: [
+    {
+      files: ['*.config.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ],
+  rules: {
+    // Prettier rule
+    'prettier/prettier': ['error', prettierConfig],
+    // Disabled rules from presets
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/prop-types': 'off',
+    // React hooks rules
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
+}
