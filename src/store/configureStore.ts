13b68ee8ec3f094c23f3121c217d0c5adb1369b7
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-03-17 10:43:59 +0200

    Add redux-starter-kit

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
+import { configureStore as configureStoreOriginal } from 'redux-starter-kit/src/configureStore'
 
 import { rootReducer } from './reducers/'
 import { AppState } from './state/App.state'
 
 export function configureStore(preloadedState?: AppState) {
-  let store = createStore(rootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk)))
+  const store = configureStoreOriginal({
+    reducer: rootReducer,
+    preloadedState
+  })
 
   if (module.hot) {
     module.hot.accept('./reducers/', () => {
