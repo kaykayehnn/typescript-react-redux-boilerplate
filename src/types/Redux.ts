ba49e6cc80a86f3cf731b982adb09acc69d000ea
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-03-29 13:38:21 +0200

    Revert print width to 80 (default)

--- src/types/Redux.ts
+++ src/types/Redux.ts
@@ -1,11 +1,15 @@
 import { Store as StoreOriginal, Action, AnyAction } from 'redux'
 import { ThunkDispatch, ThunkAction as ThunkActionOriginal } from 'redux-thunk'
-import { MapDispatchToProps as MapDispatchToPropsOriginal, ResolveThunks } from 'react-redux'
+import {
+  MapDispatchToProps as MapDispatchToPropsOriginal,
+  ResolveThunks
+} from 'react-redux'
 
 import { AppState } from '@Store/state/App.state'
 import { AppActionTypes } from '@Store/actions'
 
-export interface Store<S, A extends Action = AnyAction> extends StoreOriginal<S, A> {
+export interface Store<S, A extends Action = AnyAction>
+  extends StoreOriginal<S, A> {
   dispatch: ThunkDispatch<S, never, A>
 }
 
@@ -14,12 +18,17 @@ export type MapStateToProps<TStateProps, TOwnProps = {}> = (
   ownProps?: TOwnProps
 ) => TStateProps
 
-export type MapDispatchToProps<TDispatchProps, TOwnProps = {}> = MapDispatchToPropsOriginal<
+export type MapDispatchToProps<
   TDispatchProps,
-  TOwnProps
->
+  TOwnProps = {}
+> = MapDispatchToPropsOriginal<TDispatchProps, TOwnProps>
 
 export type MergeProps<TStateProps, TDispatchProps = {}> = TStateProps &
   ResolveThunks<TDispatchProps>
 
-export type ThunkAction<R> = ThunkActionOriginal<R, AppState, undefined, AppActionTypes>
+export type ThunkAction<R> = ThunkActionOriginal<
+  R,
+  AppState,
+  undefined,
+  AppActionTypes
+>
