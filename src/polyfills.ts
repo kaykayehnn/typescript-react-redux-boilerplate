3a3910ae35a9d9595c552fb6732b1ed30a36c8f9
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-04-05 19:52:39 +0300

    Change default polyfills, remove core-js

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,2 +1,6 @@
-import 'core-js/es/map'
-import 'core-js/es/set'
+require('es6-promise/auto')
+require('unfetch')
+// For a full fetch polyfill install whatwg-fetch
+// require('whatwg-fetch')
+
+export {}
