1ce73ba79b4063c6eeae22366d420a1b53a96e0c
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-04-06 06:55:46 +0300

    Add postcss-normalize

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
+    require('postcss-normalize')({ forceImport: true }),
     process.env.NODE_ENV === 'production' &&
       require('cssnano')({
         preset: [
