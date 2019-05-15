bf91ec6a50c59634d48c10674e36ce74760706ac
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-05-15 18:30:45 +0300

    Add component props interface to its definition
    Fixes dumb

--- src/components/Component/Component.component.tsx
+++ src/components/Component/Component.component.tsx
@@ -2,6 +2,6 @@ import React, { FunctionComponent } from 'react'
 
 export interface ComponentProps {}
 
-export const Component: FunctionComponent = () => {
+export const Component: FunctionComponent<ComponentProps> = () => {
   return <h1>Hello world</h1>
 }
